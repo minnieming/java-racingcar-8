dabb1f15d89561743fa41d2896a7ce52c4ff5a62
LeeEunmi <eunmilee9109.1@gmail.com>
2025-10-27 00:48:40 +0900

    feat(test): 자동차 쉼표 구분하는 기능 테스트 코드
    자동차 쉼표로 구분하는 기능 테스트 코드를 작성했습니다.

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -22,4 +22,23 @@ class CarTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessages.NAME_TOO_LONG);
     }
+
+    // 02. parseCars() 테스트
+    @Test
+    void 정상값_쉼표기준_자동차_목록() {
+        List<Car> cars = Car.parseCars("pobi, woni, java");
+        assertThat(cars).hasSize(3);
+        assertThat(cars.get(0).getName()).isEqualTo("pobi");
+        assertThat(cars.get(1).getName()).isEqualTo("woni");
+        assertThat(cars.get(2).getName()).isEqualTo("java");
+    }
+
+    @Test
+    void 공백_포함() {
+        List<Car> cars = Car.parseCars("  pobi  ,   woni ");
+        assertThat(cars).hasSize(2);
+        assertThat(cars.get(0).getName()).isEqualTo("pobi");
+        assertThat(cars.get(1).getName()).isEqualTo("woni");
+    }
+
 }
